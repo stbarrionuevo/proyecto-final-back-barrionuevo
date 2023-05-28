eac0723f9abc85abfe4986c989029b94bb744c87
Santiago Barrionuevo <santiagobarrionuevo27@gmail.com>
2023-05-27 22:40:41 -0300

    first commit bases del proyecto

--- src/graphql/resolvers/products/mutations.js
+++
@@ -1,7 +0,0 @@
-const { addNewProductDto, delProductByIdDto, modifyProductByIdDto } = require("../../../DTO/productDto");
-
-module.exports.productMutations = {
-    addProduct: async(_, { productToAdd }) => await addNewProductDto ( productToAdd ),
-    delProductById: async(_, { productId } ) => await delProductByIdDto ( productId.id ),
-    modifyProductById: async(_, { producToModify }) => await modifyProductByIdDto ( productToModify.id, producToModify )
-}
